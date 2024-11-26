c354c36c984138de55fac47886c7add96d335bcb
Aluno <lale7ra@gmail.com>
2024-11-25 21:15:23 -0300

    eu nao sei

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Task.Models;
+
+namespace Task.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private static List<ModelTask> modelTasks = new List<ModelTask>();
+
+        [HttpGet]
+        public ActionResult<List<ModelTask>> SearchTask()
+        {
+            return Ok(modelTasks);
+        }
+
+        [HttpPost]
+        public ActionResult<ModelTask> AddTask(ModelTask newTask)
+        {
+            if (newTask.Description.Length < 10)
+            {
+                return BadRequest("A descrição deve ter pelo menos 10 caracteres.");
+            }
+
+            newTask.Id = modelTasks.Count > 0 ? modelTasks[modelTasks.Count - 1].Id + 1 : 1;
+            modelTasks.Add(newTask);
+            return Ok(newTask);
+        }
+
+
+        [HttpDelete("{id}")]
+        public ActionResult<ModelTask> DeleteTask(int id)
+        {
+            var task = modelTasks.FirstOrDefault(t => t.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            modelTasks.Remove(task);
+            return Ok(task);
+        }
+
+
+    }
+}
